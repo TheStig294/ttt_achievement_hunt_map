44a0114ae457fc760390bcd42eca0c5ca90e02c8
The Stig <TheStig294@users.noreply.github.com>
2023-03-19 16:05:54 +1030

    Printing first 10 frames to console

--- lua/autorun/ttt_achievement_hunt_ascii_star_wars.lua
+++ lua/autorun/ttt_achievement_hunt_ascii_star_wars.lua
@@ -1,6 +1,23 @@
 if not ((game.GetMap() == "ttt_achievement_hunt" or game.GetMap() == "ttt_achievement_hunt_final") and engine.ActiveGamemode() == "terrortown") then return end
+if not CLIENT then return end
 local f = file.Open("lua/autorun/ttt_achievement_hunt_ascii.lua", "r", "GAME")
-print(f:ReadLine())
-print(f:ReadLine())
-print(f:Tell())
+local frameLines = 13
+print("=====")
+-- Skip past the initial lua comment out line: "--[["
+f:Skip(6)
+
+local function PrintFrame()
+    for i = 1, frameLines + 1 do
+        if i == 1 then
+            print("Hold seconds:", f:ReadLine())
+        else
+            print(f:ReadLine())
+        end
+    end
+end
+
+for i = 1, 10 do
+    PrintFrame()
+end
+
 f:Close()
