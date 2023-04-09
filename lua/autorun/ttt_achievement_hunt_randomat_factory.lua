9922ea3852627a916475e4445c7a6f9b4a01d5e2
The Stig <TheStig294@users.noreply.github.com>
2023-04-09 19:27:11 +0930

    Made randomat naming window auto-clear text when you click into the text box

--- lua/autorun/ttt_achievement_hunt_randomat_factory.lua
+++ lua/autorun/ttt_achievement_hunt_randomat_factory.lua
@@ -194,7 +194,12 @@ if CLIENT then
         local nameHeight = 25
         NameEntry:SetPos(width / 2 - nameWidth / 2, height / 2 - nameHeight / 2)
         NameEntry:SetSize(nameWidth, nameHeight)
-        NameEntry:SetText("(name)")
+        NameEntry:SetText("(Enter name here)")
+
+        -- Make the text field clear when you click into it.
+        NameEntry.OnGetFocus = function(self)
+            self:SetText("")
+        end
 
         NameEntry.OnEnter = function(self)
             randomatName = self:GetValue()
